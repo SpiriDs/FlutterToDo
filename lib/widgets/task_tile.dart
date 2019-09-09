ab2a720f10775de135890e348061fdf760b54082
SpiriDs <spirids@live.de>
2019-09-09 21:25:05 +0700

    Implement global State with stateful widget

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  //! checkBoxCallback getting an anonymous CallBackFunction without a name
+  // void checkBoxCallback(bool checkBoxState) {
+  //   setState(() {
+  //     isChecked = checkBoxState;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool newValue) {
-          //TODO Implement functionality
-        },
+      title: Text(
+        'Task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      //! checkBoxCallback getting an anonymous CallBackFunction at toogleCheckboxState
+      trailing: TaskCheckBox(
+          checkboxState: isChecked,
+          toogleCheckboxState: (bool checkBoxState) {
+            setState(() {
+              isChecked = checkBoxState;
+            });
+          }),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toogleCheckboxState;
+
+  TaskCheckBox({this.checkboxState, this.toogleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: toogleCheckboxState,
     );
   }
 }
