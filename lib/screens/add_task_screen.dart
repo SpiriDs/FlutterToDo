45295b77f2704ff9700a0d818aeb3b63741f23fa
SpiriDs <spirids@live.de>
2019-09-09 19:21:19 +0700

    Complete the design of the todo list

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,12 +6,41 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(30),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(20.0),
             topRight: Radius.circular(20.0),
           ),
           color: Colors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Add Task',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.lightBlueAccent,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            FlatButton(
+              color: Colors.lightBlueAccent,
+              textColor: Colors.white,
+              child: Text('Add'),
+              onPressed: () {
+                //TODO Implement functionality
+              },
+            )
+          ],
         ),
       ),
     );
