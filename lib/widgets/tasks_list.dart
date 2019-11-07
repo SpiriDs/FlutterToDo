1e8c8ccb320d65ed5149149b7f1d1750c3577fc7
SpiriDs <spirids@live.de>
2019-11-07 15:04:53 +0700

    Implement the Consumer Widget in the task_list.dart file

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,20 +6,25 @@ import 'package:provider/provider.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: Provider.of<TaskData>(context).tasks.length,
-      itemBuilder: (BuildContext context, int index) {
-        return TaskTile(
-          taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-          //! Here is the anonymous callback function from the task_list.dart ended up after 175 :)
-          checkboxCallback: (checkboxState) {
-            print('Callback run');
-            /* setState(
-              () {
-                widget.tasks[index].toggleDone();
+    //! Implement the Consumer Widget to substitude the Provider.of
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemCount: taskData.tasks.length,
+          itemBuilder: (BuildContext context, int index) {
+            return TaskTile(
+              taskTitle: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              //! Here is the anonymous callback function from the task_list.dart ended up after 175 :)
+              checkboxCallback: (checkboxState) {
+                print('Callback run');
+                /* setState(
+                  () {
+                    widget.tasks[index].toggleDone();
+                  },
+                ); */
               },
-            ); */
+            );
           },
         );
       },
